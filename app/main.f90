cf84f49afd8a91b1a08905cf60c63e2bd82b74ac
Austin C Bullock <acb.bullock@gmail.com>
2022-12-16 17:34:32 -0700

    Modified tests
    Simplified main program and included sample data for (anti-)ferromagnetic scenarios.

--- app/main.f90
+++ app/main.f90
@@ -2,34 +2,22 @@
 !!  This program demonstrates the use of the nnqs module.
 !!---------------------------------------------------------------------------------------------------------------------
 program main
-    use, intrinsic :: iso_fortran_env, only: rk=>real64                                        !! Import standard kinds
-    use io_fortran_lib, only: to_file                                                   !! I/O procedures and constants
+    use, intrinsic :: iso_fortran_env, only: rk=>real64
     use nnqs, only: RestrictedBoltzmannMachine                                                        !! Neural network
     implicit none (type,external)                                                    !! No implicit types or interfaces
 
     !! Variable Declarations ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     type(RestrictedBoltzmannMachine) :: psi                                                           !! Neural network
-
     integer :: spins, hidden_units                                                  !! Number of spins and hidden units
-    real(rk) :: ising_strengths(2)                                          !! Coupling strength J and field strength B
-    real(rk), allocatable, dimension(:,:) :: energies, correlations                        !! Energies and correlations
 
     !! Begin Executable Code ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     call random_init(repeatable=.false., image_distinct=.true.)                   !! Initialize random number generator
 
     spins = 1001                                                                         !! Set number of visible units
     hidden_units = 50                                                                     !! Set number of hidden units
-    ising_strengths = [0.5_rk, 0.1_rk]                                  !! Set coupling strength J and field strength B
 
     psi = RestrictedBoltzmannMachine(v_units=spins, h_units=hidden_units)                            !! Create instance
 
-    call psi%stochastic_optimization( ising_strengths=ising_strengths, &                                 !! Input [J,B]
-                                      energies=energies, &                                     !! Output energies array
-                                      correlations=correlations )                          !! Output correlations array
-
-    if ( this_image() == 1 ) then                                                                  !! Do I/O on image 1
-        call to_file(energies, file_name='./data/energies.csv', header=['Energy', 'Error'])
-        call to_file(correlations, file_name='./data/correlations.csv', header=['Epoch'])
-    end if
+    call psi%stochastic_optimization(ising_strengths=[ 0.5_rk, 0.1_rk ])                                 !! Input [J,B]
 
 end program main
